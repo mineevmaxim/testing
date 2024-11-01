e5990db9eb129370f782d3bea03034ca644307fc
Maxim Mineev <mms1401@ya.ru>
2024-11-01 22:34:57 +0500

    ObjectComparison. Рефакторинг второго теста, комментарии кода

--- cs/HomeExercises/ObjectComparison.cs
+++ cs/HomeExercises/ObjectComparison.cs
@@ -3,88 +3,87 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class ObjectComparison
-	{
-		private Person actualTsar;
-		private Person expectedTsar;
+    public class ObjectComparison
+    {
+        private Person actualTsar;
+        private Person expectedTsar;
 
-		[SetUp]
-		public void Setup()
-		{
-			actualTsar = TsarRegistry.GetCurrentTsar();
-			expectedTsar = CreateDefaultTsar();
-		}
-		
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			actualTsar.Name.Should().Be(expectedTsar.Name);
-			actualTsar.Age.Should().Be(expectedTsar.Age);
-			actualTsar.Height.Should().Be(expectedTsar.Height);
-			actualTsar.Weight.Should().Be(expectedTsar.Weight);
+        [SetUp]
+        public void Setup()
+        {
+            actualTsar = TsarRegistry.GetCurrentTsar();
+            expectedTsar = CreateDefaultTsar();
+        }
 
-			actualTsar.Parent!.Name.Should().Be(expectedTsar.Parent!.Name);
-			actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
-			actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
-			actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
-		}
+        [Test]
+        [Description("Проверка текущего царя")]
+        [Category("ToRefactor")]
+        public void CheckCurrentTsar()
+        {
+            actualTsar.Name.Should().Be(expectedTsar.Name);
+            actualTsar.Age.Should().Be(expectedTsar.Age);
+            actualTsar.Height.Should().Be(expectedTsar.Height);
+            actualTsar.Weight.Should().Be(expectedTsar.Weight);
 
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			// Какие недостатки у такого подхода? 
-			// При добавлении нового поля в класс Person, нам придется залезать и править функцию AreEqual
-			Assert.True(AreEqual(actualTsar, expectedTsar));
-		}
+            actualTsar.Parent!.Name.Should().Be(expectedTsar.Parent!.Name);
+            actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
+            actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
+            actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
+        }
 
-		private static bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent);
-		}
-		
-		private static Person CreateDefaultTsar() 
-			=> new Person("Ivan IV The Terrible", 54, 170, 70, CreateDefaultParent());
+        [Test]
+        [Description("Альтернативное решение. Какие у него недостатки?")]
+        public void CheckCurrentTsar_WithCustomEquality()
+        {
+            // Какие недостатки у такого подхода? 
+            // AreEqual не содержит в себе Assert, при падении теста мы не знаем, что произошло (неинформативный вывод)
+            // При расширении класса Person, нам необходимо менять код уже написанного метода AreEqual
+            // и компилятор нам не подскажет, что это нужно сделать
+            // В тесте только вызывается метод AreEqual, приходится писать инфраструктурный код для его создания
 
-		private static Person CreateDefaultParent() 
-			=> new Person("Vasili III of Russia", 28, 170, 60, null);
-	}
+            // Плюсы моего подхода:
+            // При расширении класса Person, программа не скомпилируется, если не поменять код (предотвращает ошибки).
+            // Код приходится менять только в генераторах объектов
+            // При расширении класса Person, нам не нужно изменять тест (первый тест придется, компилятор не подскажет)
+            // Логика сравнения перенесена в сам тест, не приходится писать лишний инфраструктурный код
+            // P.S. создание объектов вынесено в Setup. Мы не сравниваем Id, создавать каждый раз новые данные для теста нет необходимости
+            actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+                options.Excluding(y => y.Id).Excluding(y => y.Parent.Id));
+        }
 
-	public class TsarRegistry
-	{
-		public static Person GetCurrentTsar()
-		{
-			return new Person(
-				"Ivan IV The Terrible", 54, 170, 70,
-				new Person("Vasili III of Russia", 28, 170, 60, null));
-		}
-	}
+        private static Person CreateDefaultTsar()
+            => new Person("Ivan IV The Terrible", 54, 170, 70, CreateDefaultParent());
 
-	public class Person
-	{
-		public static int IdCounter = 0;
-		public int Age, Height, Weight;
-		public string Name;
-		public Person? Parent;
-		public int Id;
+        private static Person CreateDefaultParent()
+            => new Person("Vasili III of Russia", 28, 170, 60, null);
+    }
 
-		public Person(string name, int age, int height, int weight, Person? parent)
-		{
-			Id = IdCounter++;
-			Name = name;
-			Age = age;
-			Height = height;
-			Weight = weight;
-			Parent = parent;
-		}
-	}
+    public class TsarRegistry
+    {
+        public static Person GetCurrentTsar()
+        {
+            return new Person(
+                "Ivan IV The Terrible", 54, 170, 70,
+                new Person("Vasili III of Russia", 28, 170, 60, null));
+        }
+    }
+
+    public class Person
+    {
+        public static int IdCounter = 0;
+        public int Age, Height, Weight;
+        public string Name;
+        public Person? Parent;
+        public int Id;
+
+        public Person(string name, int age, int height, int weight, Person? parent)
+        {
+            Id = IdCounter++;
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+            Parent = parent;
+        }
+    }
 }
